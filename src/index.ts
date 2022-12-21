8bf45e1a9f1391e4ef0085585a9ec49e8086d6ca
Pavel Demaev <ctpater@gmail.com>
2022-12-21 19:43:48 +0300

    feat: configire tsconfig.json for json-file, add products.json

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import './style.css';
+import data from './assets/data/products.json';
 
-function component(text: string) {
-    const element = document.createElement('h1');
-    element.textContent = text;
-    return element;
-}
+// function component(text: string) {
+//     const element = document.createElement('h1');
+//     element.textContent = text;
+//     return element;
+// }
 
 // document.body.prepend(component('Проект собран на Webpack'));
+console.log(data.products);
